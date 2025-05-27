babcec97a00b2ae69f974018663363f5ff621bb9
Suon Samoun <suonsamoun@gmail.com>
2025-05-27 10:27:44 +0700

    feat: added redis-commander

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Tw@bfYCNh@J2';
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
